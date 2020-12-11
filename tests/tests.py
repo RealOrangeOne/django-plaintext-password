0a955f23ffb5994163ec36df99fe5e0d0742b869
Jake Howard <git@theorangeone.net>
2020-12-11 19:50:15 +0000

    Add a test for dollars being in password

--- tests/tests.py
+++ tests/tests.py
@@ -8,3 +8,8 @@ def test_makes_plaintext_password():
 
 def test_check_password():
     assert check_password("password", "plaintext$$password")
+
+
+def test_check_password_with_unsafe_char():
+    hashed_password = make_password("$password$", hasher="plaintext")
+    assert check_password("$password$", hashed_password)
