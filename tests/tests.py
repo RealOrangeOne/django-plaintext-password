f12d91f83a1ba653f828354ddae3c24fa1c42cef
Jake Howard <git@theorangeone.net>
2020-12-11 20:06:45 +0000

    Add some more tests, powered by hypothesis

--- tests/tests.py
+++ tests/tests.py
@@ -1,15 +1,33 @@
-from django.contrib.auth.hashers import check_password, make_password
+import pytest
+from django.contrib.auth.hashers import check_password, identify_hasher, make_password
+from hypothesis import given
+from hypothesis.strategies import text
+
+from plaintext_password import PlaintextPasswordHasher
 
 
-def test_makes_plaintext_password():
-    hashed_password = make_password("password", hasher="plaintext")
-    assert hashed_password == "plaintext$$password"
+@given(text())
+def test_makes_plaintext_password(password):
+    hashed_password = make_password(password, hasher="plaintext")
+    assert hashed_password == f"plaintext$${password}"
 
 
-def test_check_password():
-    assert check_password("password", "plaintext$$password")
+@given(text())
+def test_check_password(password):
+    assert check_password(password, f"plaintext$${password}")
 
 
-def test_check_password_with_unsafe_char():
-    hashed_password = make_password("$password$", hasher="plaintext")
-    assert check_password("$password$", hashed_password)
+@pytest.mark.parametrize("password", {"$password$", "!password!"})
+def test_check_password_with_unsafe_char(password):
+    hashed_password = make_password(password, hasher="plaintext")
+    assert check_password(password, hashed_password)
+
+
+@given(text())
+def test_end_to_end(password):
+    hashed_password = make_password(password, hasher="plaintext")
+    assert check_password(password, hashed_password)
+
+
+def test_identify_hasher():
+    assert isinstance(identify_hasher("plaintext$$password"), PlaintextPasswordHasher)
