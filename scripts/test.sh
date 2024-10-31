8ccbb37e3eb3532154bbaf2cc069f1b0c678fe42
Jake Howard <git@theorangeone.net>
2024-10-31 09:00:34 +0000

    General Django, Python and dependency updates

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,11 +8,7 @@ set -ex
 
 pytest --verbose --cov plaintext_password/ --cov-report term --cov-report html --benchmark-sort=mean --benchmark-columns=min,max,mean,stddev,ops --benchmark-group-by=func
 
-if hash black 2>/dev/null;
-then
-    black plaintext_password tests setup.py --check
-fi
-
 ruff check plaintext_password tests setup.py
+ruff format --check plaintext_password tests setup.py
 
 mypy plaintext_password tests setup.py
